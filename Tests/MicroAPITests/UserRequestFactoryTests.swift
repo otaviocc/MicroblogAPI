2200c3fb88f525a60a1d2b7ac00b84f5e46b44c9
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2021-10-24 20:17:19 +0200

    Add UserRequestFactoryTests

--- Tests/MicroAPITests/UserRequestFactoryTests.swift
+++ Tests/MicroAPITests/UserRequestFactoryTests.swift
@@ -49,4 +49,132 @@ final class UserRequestFactoryTests: XCTestCase {
         XCTAssertNil(request.decoder)
         XCTAssertNil(request.encoder)
     }
+
+    func testMakeFollowingRequest() throws {
+        let request = UserRequestFactory.makeFollowingRequest(
+            username: "some_username"
+        )
+
+        XCTAssertEqual(
+            request.path, "/users/following/some_username"
+        )
+
+        XCTAssertEqual(
+            request.method,
+            .get
+        )
+
+        XCTAssertNil(request.queryItems)
+        XCTAssertNil(request.body)
+        XCTAssertNil(request.decoder)
+        XCTAssertNil(request.encoder)
+    }
+
+    func testMakeFollowRequest() throws {
+        let request = UserRequestFactory.makeFollowRequest(
+            username: "some_username"
+        )
+
+        XCTAssertEqual(
+            request.path, "/users/follow"
+        )
+
+        XCTAssertEqual(
+            request.method,
+            .post
+        )
+
+        XCTAssertTrue(
+            try XCTUnwrap(
+                request.queryItems?.contains(
+                    URLQueryItem(name: "username", value: "some_username")
+                )
+            )
+        )
+
+        XCTAssertNil(request.body)
+        XCTAssertNil(request.decoder)
+        XCTAssertNil(request.encoder)
+    }
+
+    func testMakeMuteRequest() throws {
+        let request = UserRequestFactory.makeMuteRequest(
+            username: "some_username"
+        )
+
+        XCTAssertEqual(
+            request.path, "/users/mute"
+        )
+
+        XCTAssertEqual(
+            request.method,
+            .post
+        )
+
+        XCTAssertTrue(
+            try XCTUnwrap(
+                request.queryItems?.contains(
+                    URLQueryItem(name: "username", value: "some_username")
+                )
+            )
+        )
+
+        XCTAssertNil(request.body)
+        XCTAssertNil(request.decoder)
+        XCTAssertNil(request.encoder)
+    }
+
+    func testMakeReportRequest() throws {
+        let request = UserRequestFactory.makeReportRequest(
+            username: "some_username"
+        )
+
+        XCTAssertEqual(
+            request.path, "/users/report"
+        )
+
+        XCTAssertEqual(
+            request.method,
+            .post
+        )
+
+        XCTAssertTrue(
+            try XCTUnwrap(
+                request.queryItems?.contains(
+                    URLQueryItem(name: "username", value: "some_username")
+                )
+            )
+        )
+
+        XCTAssertNil(request.body)
+        XCTAssertNil(request.decoder)
+        XCTAssertNil(request.encoder)
+    }
+
+    func testMakeUnfollowRequest() throws {
+        let request = UserRequestFactory.makeUnfollowRequest(
+            username: "some_username"
+        )
+
+        XCTAssertEqual(
+            request.path, "/users/unfollow"
+        )
+
+        XCTAssertEqual(
+            request.method,
+            .post
+        )
+
+        XCTAssertTrue(
+            try XCTUnwrap(
+                request.queryItems?.contains(
+                    URLQueryItem(name: "username", value: "some_username")
+                )
+            )
+        )
+
+        XCTAssertNil(request.body)
+        XCTAssertNil(request.decoder)
+        XCTAssertNil(request.encoder)
+    }
 }
