82bca91a39a09c8d52e9d99b0fd4c4983c3fec96
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2021-11-01 12:27:02 +0100

    Simplify URLResponse.location

--- Sources/MicroAPI/Extensions/URLResponse.swift
+++ Sources/MicroAPI/Extensions/URLResponse.swift
@@ -3,13 +3,8 @@ import Foundation
 extension URLResponse {
 
     public var location: URL? {
-        guard let httpResponse = self as? HTTPURLResponse,
-              let location = httpResponse.value(forHTTPHeaderField: "Location"),
-              let url = URL(string: location)
-        else {
-            return nil
-        }
-
-        return url
+        (self as? HTTPURLResponse)?
+            .value(forHTTPHeaderField: "Location")
+            .flatMap(URL.init(string:))
     }
 }
